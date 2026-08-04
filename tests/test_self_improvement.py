f44b3144153b4bb118c70ca3c00f1e786f117417
Mr-Nobody-Anonymous <bmx310712@gmail.com>
2026-08-04 10:09:04 +0300

    test

--- tests/test_self_improvement.py
+++ tests/test_self_improvement.py
@@ -10,7 +10,12 @@ from self_improvement.improvement_loop import SelfImprovementLoop
 
 
 def run_async(coro):
-    return asyncio.get_event_loop().run_until_complete(coro)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        return loop.run_until_complete(coro)
+    finally:
+        loop.close()
 
 
 class TestTelemetryCollector:
